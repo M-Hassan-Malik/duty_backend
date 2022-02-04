51ec9709c3b2c97319a8ec69def35d7ffd3b381e
M-Hassan-Malik <mhassan.malik.1997@gmail.com>
2022-02-04 06:40:57 -0800

    feb4-1

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ require("dotenv").config();
 var cors = require("cors");
 
 app.use(
-	cors({
-		origin: "*",
-		credentials: true,
-	})
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
 
 fsAdmin.initializeApp({
-	credential: fsAdmin.credential.cert(serviceAccount),
+  credential: fsAdmin.credential.cert(serviceAccount),
 });
 
 app.use(express.json());
@@ -24,19 +24,56 @@ app.use("/user", require("./api/user"));
 app.use("/duty", require("./api/duty"));
 app.use("/duty_history", require("./api/duty_history"));
 
+// exec(
+//   "lt --port 3000 --subdomain 'anythinkyouwantsir'",
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(`error: ${error.message}`);
+//       return;
+//     }
+//     if (stderr) {
+//       console.log(`stderr: ${stderr}`);
+//       return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//   }
+// );
 
-exec("lt --port 3000 --subdomain 'anythinkyouwantsir'", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+// fsAdmin
+//   .firestore()
+//   .collection("duty")
+//   .doc("Pakistan")
+//   .collection("Karachi City")
+//   .onSnapshot((doc) => {
+//       if (doc) {
+//           console.log('ayaaaaaaNew', doc)
+//       }
+//     });
+
+
+// fsAdmin
+//   .firestore()
+//   .collection("duty")
+//   .doc("Pakistan")
+//   .collection("Karachi City")
+//   .doc("active_duties")
+//   .collection("task")
+//   .where("duty.done", "==", 0)
+//   .onSnapshot((querySnapshot) => {
+//     querySnapshot.docChanges().forEach((change) => {
+//       if (change.type === "modified") {
+//         console.log("Modified user's id: ", change.doc.data().uid);
+//       }
+//     });
+//   });
+
+//.doc("XS0h9n0VE4N4KNw6JsMR")
+// .onSnapshot((doc) => {
+//   if (doc) {
+//       console.log('ayaaaaaaNew', doc.data())
+//   }
+// });
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`Server is running on localhost:3000`);
+  console.log(`Server is running on localhost:3000`);
 });
